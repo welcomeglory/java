a74b76b92c5dfd3b22cc2447f2c527246381e699
sjy <sjy@DESKTOP-3QLVGT6>
2024-01-17 11:38:14 +0900

    0117

--- src/java_0117/test22.java
+++ src/java_0117/test22.java
@@ -6,8 +6,6 @@ public class test22 {
 			      {22, 33},
 			      {44, 55, 66}
 			   };
-		//dddl
-		
 		for(int i=0; i<arr.length;i++ ) {
 			for(int j=0; j<arr[i].length;j++)
 				System.out.print(arr[i][j]+"\t");
