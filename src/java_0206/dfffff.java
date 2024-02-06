069f53c31f841ef03d3586e9680ca7f13c1645d2
welcomeglory <godbu2021@naver.com>
2024-02-06 10:26:21 +0900

    1

---
+++ src/java_0206/dfffff.java
@@ -0,0 +1,21 @@
+package java_0206;
+
+import java.util.Arrays;
+
+public class dfffff {
+	public static void main(String[] args) {
+		int[] num_list = {12, 4, 15, 46, 38, 1, 14};
+	       // int[] answer = new int[5];
+	        int emp=0;
+	        Arrays.sort(num_list);
+
+        for(int i=0; i<5; i++){
+	        	System.out.println(i+"\t"+num_list[i]);
+	        }
+//	        int index=0;
+//	        for (int element : num_list) {
+//	            answer[index++] = element;      
+//	        }   
+        }
+}
+
